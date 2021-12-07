d2dd2147f0a7e4730c90749aa373b20c5347508d
Keith Staines <keith.staines@workfinder.com>
2021-12-07 06:01:19 +0000

    Initial Commit

---
+++ ImageDownloader/ImageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloaderApp.swift
+//  ImageDownloader
+//
+//  Created by Keith Staines on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
